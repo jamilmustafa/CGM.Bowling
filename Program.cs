c8f13434878a49470f201dc847fc17124e6a39fd
Jamil <jamil.mustafa@training4u.it>
2021-07-12 17:31:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CGM.Bowling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
